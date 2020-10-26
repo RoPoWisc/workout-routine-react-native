807cb2be993a849d1090b0fe90794c592da12534
Andrew Harron <aharron@wisc.edu>
2020-10-26 16:54:08 -0500

    data rendering from lists now in workout screen

--- screens/StartWorkout.js
+++ screens/StartWorkout.js
@@ -10,15 +10,43 @@ import {
   IconRegistry,
   Layout,
   Text,
-  CheckBox
+  CheckBox,
+  List
 } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
 
-var exercise_array = [
+var squat_array = [
     {weight: 225, reps: 8},
     {weight: 235, reps: 5}
 ]
+
+var squat = {
+    name: 'Squat',
+    totalVolume: 'Total Volume',
+    sets: squat_array
+}
+
+var legpress_array = [
+    {weight: 405, reps: 10},
+    {weight: 335, reps: 12},
+    {weight: 300, reps: 14}
+]
+
+var legpress = {
+    name: 'Leg Press',
+    totalVolume: 'Total Volume',
+    sets: legpress_array
+}
+
+var workout = {
+    routineName: 'Workout A',
+    routineDay: 'Leg Day',
+    exerciseArray: [
+        squat,
+        legpress
+    ]
+}
 
 const MenuIcon = (props) => (
     <Icon {...props} name='menu-outline'/>
@@ -39,48 +67,60 @@ const Checkbox = () => {
     );
 };
 
-const ExerciseEntry = (exercise_entry) => (
-    <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        backgroundColor: '#0466C880',
-        borderRadius: 8,
-        paddingTop: 3,
-        paddingBottom: 3,
-        marginVertical: 3
-    }}>
-        <Text category='s1' style={exerciseStyles.weight}>{exercise_entry.weight}</Text>
-        <Text category='s1' style={exerciseStyles.reps}>{exercise_entry.reps}</Text>
-        <Checkbox style={{}}></Checkbox>
-    </View>
-);
+const ExerciseEntry = (exercise_entry) => {
 
-const Exercise = (props) => (
-    <View style={exerciseStyles.exercise}>
-        <View style={{
-            justifyContent: 'space-between',
-            flexDirection: 'row',
-            borderBottomColor: '#002855',
-            borderBottomWidth: 1,
-        }}>
-            <Text category='s1' style={exerciseStyles.header}>Squat</Text>
-            <Text category='s1' style={exerciseStyles.header}>Total Volume</Text>
-        </View>
-
+    return (
         <View style={{
             flexDirection: 'row',
             justifyContent: 'space-between',
+            backgroundColor: '#0466C880',
+            borderRadius: 8,
+            paddingTop: 3,
+            paddingBottom: 3,
+            marginVertical: 3
         }}>
-            <Text category='s1' style={exerciseStyles.exerciseInfo}>Weight</Text>
-            <Text category='s1' style={exerciseStyles.exerciseInfo}>Reps</Text>
-            <Text category='s1' style={exerciseStyles.exerciseInfo}>Set Complete?</Text>
+            <Text category='s1' style={exerciseStyles.weight}>{exercise_entry.weight}</Text>
+            <Text category='s1' style={exerciseStyles.reps}>{exercise_entry.reps}</Text>
+            <Checkbox style={{}}></Checkbox>
         </View>
-        <ExerciseEntry></ExerciseEntry>
-        <ExerciseEntry></ExerciseEntry>
+    );
+};
+
+const Exercise = (exercise_object) => {
+
+    return (
+        <View style={exerciseStyles.exercise}>
+            <View style={{
+                justifyContent: 'space-between',
+                flexDirection: 'row',
+                borderBottomColor: '#002855',
+                borderBottomWidth: 1,
+            }}>
+                <Text category='s1' style={exerciseStyles.header}>{exercise_object.name}</Text>
+                <Text category='s1' style={exerciseStyles.header}>{exercise_object.totalVolume}</Text>
+            </View>
+
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+            }}>
+                <Text category='s1' style={exerciseStyles.exerciseInfo}>Weight</Text>
+                <Text category='s1' style={exerciseStyles.exerciseInfo}>Reps</Text>
+                <Text category='s1' style={exerciseStyles.exerciseInfo}>Set Complete?</Text>
+            </View>
+
+            <List
+                style={{backgroundColor: '#E5E5E5'}}
+                data={exercise_object.sets}
+                renderItem={({ item }) => (
+                    <ExerciseEntry weight={item.weight} reps={item.reps}></ExerciseEntry>
+                )}
+            />
 
 
-    </View>
-);
+        </View>
+    );
+};
 
 const exerciseStyles = StyleSheet.create({
     exercise: {
@@ -119,16 +159,32 @@ class StartWorkout extends React.Component {
                     <View style={styles.header}>
                         <View style={styles.topBar}>
                             <Text style={styles.text} category='h1'>
-                                Workout A
+                                {workout.routineName}
                             </Text>
                             <Button style={styles.settingsButton} appearance='outline' accessoryLeft={MenuIcon}></Button>
                         </View>
                         <Text style={styles.name} category='h5'>
-                            Leg Day
+                            {workout.routineDay}
                         </Text>
                     </View>
-                    <Exercise></Exercise>
-                    <Exercise></Exercise>
+
+                    <List
+                        style={{
+                            backgroundColor: '#FFFFFF',
+                            width: '100%',
+                            marginLeft: 15,
+                            alignSelf: 'center'
+                        }}
+                        data={workout.exerciseArray}
+                        renderItem={({ item }) => (
+                            <Exercise 
+                                name={item.name}
+                                totalVolume={item.totalVolume}
+                                sets={item.sets}>
+
+                            </Exercise>
+                        )}
+                    />
                   </Layout>
                 </ApplicationProvider>
             </>
