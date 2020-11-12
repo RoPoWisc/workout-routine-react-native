a5396da9065795a61be216f6b908313f8fc67dfa
justinperet <jperet@wisc.edu>
2020-11-12 14:14:24 -0600

    Update login.test.js

--- tests/screens/login.test.js
+++ tests/screens/login.test.js
@@ -7,6 +7,13 @@ import {Login} from '../../screens/Login'
 describe('Login', () => {
     test('Page', () => {
         const tree = renderer.create(<Login />).toJSON();
+        //console.warn(tree);
         expect(tree.children.length).toBe(9);
     });
+    
+    test('renders', () => {
+        const render = renderer.create(<Login />).toJSON();
+        expect(render).toMatchSnapshot();
+    })
+    
 });
