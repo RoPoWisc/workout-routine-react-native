95d5ddebef43f2781976742dd92119b9f4e46c84
justinperet <jperet@wisc.edu>
2020-11-12 14:14:12 -0600

    Create settings.test.js

---
+++ tests/screens/settings.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import mockStore from 'redux-mock-store';
+
+import {Settings} from '../../screens/Settings';
+
+
+describe('Settings', () => {
+    test('Page', () => {
+        const tree = renderer.create(<Settings />).toJSON();
+        //console.warn(tree);
+        expect(tree.children.length).toBe(9);
+    });
+    
+    test('renders', () => {
+        const render = renderer.create(<Settings />).toJSON();
+        expect(render).toMatchSnapshot();
+    })
+    
+});
+
