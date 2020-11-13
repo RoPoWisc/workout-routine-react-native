c0c771fd9bab465e12ec03e6ff9822b0e7dd0f69
justinperet <jperet@wisc.edu>
2020-11-13 13:51:09 -0600

    Update register.test.js

--- tests/screens/register.test.js
+++ tests/screens/register.test.js
@@ -16,5 +16,24 @@ describe('Register', () => {
         const render = renderer.create(<Signup />).toJSON();
         expect(render).toMatchSnapshot();
     })
-    
+    let findTextElement = function(tree, element){
+        //console.warn(tree)
+        return true;
+    }
+     
+    it('Find text element', ()=>{
+       let tree = renderer.create(
+           <Signup />
+       ).toJSON();
+     
+       expect(findTextElement(tree, 'email')).toBeDefined();
+    })
+    it('check function', () => {
+        let loghand = renderer.create(<Signup />).getInstance();
+        expect(loghand.handleSignUp('ema')).toBeDefined();
+
+
+        
+    })
+
 });
