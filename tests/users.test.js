70eb7ad26df15d8ab3e837dc1cd6b14df4a4bd7b
Rohit <rohitpotineni@MacBook-Pro.local>
2020-11-13 19:12:36 -0600

    Coverage reached

--- tests/users.test.js
+++ tests/users.test.js
@@ -13,19 +13,37 @@ describe('Users.js TESTS', () => {
     it('updateEmail', () => {
         expect(updateEmail('sd').payload).toEqual('sd');
     });
+    it('updateEmail_Empty', () => {
+        expect(updateEmail().payload).toEqual();
+    });
     it('updatePassword', () => {
         expect(updatePassword('sd').payload).toEqual('sd');
     });
+    it('updatePassword_Empty', () => {
+        expect(updatePassword().payload).toEqual();
+    });
     it('updateConfirmPassword', () => {
+        expect(updateConfirmPassword('sd').payload).toEqual('sd');
+    });
+    it('updateConfirmPassword_Empty', () => {
         expect(updateConfirmPassword('sd').payload).toEqual('sd');
     });
     it('updateName', () => {
         expect(updateName('sd').payload).toEqual('sd');
     });
+    it('updateName_Empty', () => {
+        expect(updateName().payload).toEqual();
+    });
     it('fetchUserObj', () => {
         expect(fetchUserObj('sd').payload.userServer).toEqual('sd');
     });
+    it('fetchUserObj_Empty', () => {
+        expect(updateName().payload).toEqual();
+    });
     it('fetchBearerToken', () => {
         expect(fetchBearerToken('sd').payload).toEqual('sd');
+    });
+    it('fetchUserObj_Empty', () => {
+        expect(updateName().payload).toEqual();
     });
 });
